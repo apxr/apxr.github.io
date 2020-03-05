55b54b068d7461cf40526603f12108d3e610673f
Apurv Priyam <apurvpriyam@gmail.com>
2020-03-04 22:55:01 -0500

    light color mode

--- assets/js/mine.js
+++ assets/js/mine.js
@@ -56,7 +56,7 @@ $(function(){
 //  Console effect
 
 // function([string1, string2],target id,[color1,color2])    
-consoleText(['Apurv Priyam...', 'a Data Scientist...', 'a Programmer...', 'a Photographer...'], 'text',['#f4ffff','#f4ffff','#f4ffff', '#f4ffff']);
+consoleText(['Apurv Priyam...', 'a Data Scientist...', 'a Programmer...', 'a Photographer...'], 'text',['#666666','#666666','#666666', '#666666']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
